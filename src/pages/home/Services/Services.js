5ad1df721b01217a5fc80a9eef1749c86a2b9335
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2023-03-20 02:23:32 +0600

    service related work complete

--- src/pages/home/Services/Services.js
+++ src/pages/home/Services/Services.js
@@ -1,6 +1,7 @@
-import { Box } from '@mui/material';
+import { Box, Grid } from '@mui/material';
 import React from 'react'
 import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+import Service from '../../../components/Service/Service';
 
 const serviceImages = [
     'https://i.ibb.co/HNhykGp/iphone-1.png',
@@ -12,6 +13,16 @@ function Services() {
     return (
         <Box>
             <SectionTitle title={'Provide awesome'} colored={'Service'} />
+            
+            <Grid container spacing={2}>
+                {
+                    serviceImages.map((image, i) =>
+                        <Grid item xs={12} sm={6} md={4}>
+                            <Service key={i} image={image} />
+                        </Grid>)
+                }
+
+            </Grid>
         </Box>
     )
 }
