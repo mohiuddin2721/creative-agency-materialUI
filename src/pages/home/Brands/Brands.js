654cb021ecb1c74a6f36024cb739a5dcb8f163c5
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2023-03-20 01:22:30 +0600

    brands logo appear

---
+++ src/pages/home/Brands/Brands.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Box } from '@mui/material'
+import { styled } from '@mui/system';
+
+const brands = [
+    'https://i.ibb.co/27wHw8Y/image-5.png',
+    'https://i.ibb.co/vVSVd76/image-6.png',
+    'https://i.ibb.co/Dwj3KYc/image-7.png',
+    'https://i.ibb.co/99gcp3b/image-8.png',
+    'https://i.ibb.co/Rhk2VSh/image-9.png',
+ ];
+
+const BrandsWrapper = styled(Box)(({theme}) => ({
+    marginBottom: '4rem',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    '& > img' : {
+        height: '60px'
+    }
+}))
+
+function Brands() {
+  return (
+    <BrandsWrapper>
+        {
+            brands.map((brand, i) => <img key={i} src={brand} alt='Brands' />)
+        }
+    </BrandsWrapper>
+  )
+}
+
+export default Brands
