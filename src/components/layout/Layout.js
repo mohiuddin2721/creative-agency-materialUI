3d46d0ee51b5311cd930d0fecf242dfd86317f2a
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2023-03-19 23:53:27 +0600

    branding component created

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,4 +1,3 @@
-import { Outlet } from '@mui/icons-material';
 import { Box } from '@mui/material';
 import { Container } from '@mui/system';
 import React from 'react';
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Branding from './Branding/Branding';
 
 const Home = () => {
     return (
         <div>
-            home
+            <Branding />
         </div>
     );
 };
