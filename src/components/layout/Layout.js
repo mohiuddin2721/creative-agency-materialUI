bb771ab20542a67457744be27cb041b172a38020
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2023-03-19 23:47:32 +0600

    navbar designed

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,10 +1,17 @@
+import { Outlet } from '@mui/icons-material';
+import { Box } from '@mui/material';
+import { Container } from '@mui/system';
 import React from 'react';
+import Header from '../Header/Header';
 
 const Layout = () => {
     return (
-        <div>
-            Layout 
-        </div>
+        <Box>
+            <Container>
+                <Header />   
+                {/* <Outlet />  */}
+            </Container> 
+        </Box>
     );
 };
 
